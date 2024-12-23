7f7eb1ffe76e934f951f8366c97816f0aca3b128
mac <mac@macs-MacBook-Pro.local>
2024-12-23 13:04:33 -0800

    more routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use("/test/2",(req,res)=>{
+    res.send("test 22 from the server");
+});
+
 app.use("/test",(req,res)=>{
     res.send("test from the server");
 });
@@ -10,6 +14,10 @@ app.use("/hello",(req,res)=>{
     res.send("Hello from the server");
 });
 
+app.use("/",(req,res)=>{
+    res.send("from the server");
+});
+
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000...");
-});
+});  
