6b8ed9b8f7b44582290626fb26ff15a9ee72054e
mac <mac@macs-MacBook-Pro.local>
2025-05-12 16:58:58 +0530

    dev tinder

--- src/app.js
+++ src/app.js
@@ -2,21 +2,35 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test/2",(req,res)=>{
-    res.send("test 22 from the server");
+// GET /users => middlewares chain => request handler
+app.use("/",(req,res,next)=>{
+    // res.send("handling / route")
+console.log("handling the route user!!");
+next();
 });
 
-app.use("/test",(req,res)=>{
-    res.send("test from the server");
+app.get("/user",(req,res,next)=>{
+    console.log("Handling /user route");
+    next();
+},
+(req,res,next)=>{
+    res.send("1st route handler");
+},
+(req,res,next)=>{
+    res.send("2nd route handler");
 });
+// app.get("/user/:userId/:name/:password",(req,res)=>{
+//     console.log(req.params);
+//     res.send({firstName:"Akshay",lastName:"Saini"})
+// });
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello from the server");
-});
+// app.post("/user",(req,res)=>{
+//     res.send("Data succesfully saved to the database!");
+// })
 
-app.use("/",(req,res)=>{
-    res.send("from the server");
-});
+// app.use("/test",(req,res)=>{
+//     res.send("from the server!");
+// });
 
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000...");
